2d8a8ca3da11b477660a0e059bf43e9ecd3b81f9
Liana Avagyan <liana_avagyan@epam.com>
2024-01-12 15:57:54 +0400

    feat: some changes

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -3,14 +3,16 @@ import { parser } from "../helpers/parser";
 
 export const handler = async(e: any) => {
     try {
-        const { object } = e.Records[0].s3;
-        const key = decodeURIComponent(object.key.replace(/\+/g, " "));
-    
-        const csvContent = await parser('productsbucket-avlian90', key);
 
-        console.log('Parsed sucsessfully', csvContent);
-    
-    } catch {
-        throw new Error('Error');
+        for (const record of e.Records) {
+            const s3Bucket = record.s3.bucket.name;
+            const s3Key = record.s3.object.key;
+
+            const csvContent = await parser(s3Bucket, s3Key);
+
+            console.log('Parsed sucsessfully', csvContent);
+        } 
+    } catch(error) {
+        console.error(`An error occurred: ${error}`);
     }
 }
